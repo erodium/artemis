1cef61e743e32388772cd6633ded553ba45f2636
liam <ltvanion@umich.edu>
2022-02-20 23:25:41 -0500

    Get ThreatFox hostfile and parse to domains

---
+++ src/parse_domain_names.py
@@ -0,0 +1,14 @@
+with open('../data/hostfile.txt') as f:
+    lines = f.readlines()
+
+domains = []
+
+for line in lines:
+    if line[0] == "#":
+        continue
+    parts = line.strip().split()
+    if len(parts) > 1:
+        domains.append(parts[1].strip())
+
+with open('../data/domains.txt', 'w') as f:
+    f.writelines("%s\n" % d for d in domains)

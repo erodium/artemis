bde9f9366fc61472ee2138212cf69efedd23ca31
liam <ltvanion@umich.edu>
2022-04-16 18:48:06 -0400

    Replacing the try/excepts with .gets, adding a sleep, and appending to file

--- src/scripts/get_ip_data.py
+++ src/scripts/get_ip_data.py
@@ -2,7 +2,8 @@ from ipwhois import IPWhois
 import sys
 import argparse
 import json
-from parse_json import parse_json
+from src.scripts.parse_json import parse_json
+from time import sleep
 
 """
 Will use IP Whois and ASN details to identify an organization and location 
@@ -21,35 +22,29 @@ Todo:
 * NA is a country code, although not likely to show up. Should still switch to a better value for not available.
 """
 
-def obtain_ip_data(ip_address, verbose=False): 
-    results = {}
+
+def obtain_ip_data(ip_address, verbose=False):
+    cur_results = {}
     try:
         obj = IPWhois(ip_address)
-        try:
-            whois_data = obj.lookup_rdap(depth=1)
-            try: 
-                country_code = whois_data['asn_country_code']
-            except:
-                country_code = "NA"
-            try:
-                entity = whois_data['entities'][0]
-                org = whois_data['objects'][entity]['contact']['name']
-            except:
-                org = "NA"
-            results = {'CC': country_code, 'Org': org}
-        except:
-            results = {'CC': 'NA', 'Org': 'NA'}
+        whois_data = obj.lookup_rdap(depth=1, rate_limit_timeout=30)
+        country_code = whois_data.get('asn_country_code', "NA")
+        entity = whois_data.get('entities', ["NA"])[0]
+        org = whois_data.get('objects', {}).get(entity, {}).get('contact', {}).get('name', "NA")
+        cur_results = {'CC': country_code, 'Org': org}
     except:
-        results = {'CC': 'NA', 'Org': 'NA'}
-    
-    if verbose: print(results)
+        cur_results = {'CC': 'NA', 'Org': 'NA'}
+    if verbose:
+        print("Current Results:", cur_results)
+    return cur_results
 
-    return results
 
 # Allow to run as a standalone script
 if __name__ == "__main__":
     # Use Argparse to handle cli inputs
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="""
+        Make sure you have a blank output_file to start, or this will just append rows to the end if the file.
+        """)
     parser.add_argument('--dns_file', help='The dns filename to parse.')
     parser.add_argument('--output_file', help='The filename to save the results to.')
     parser.add_argument('--start_at', default=0, help='The row to start at')
@@ -61,35 +56,62 @@ if __name__ == "__main__":
     verbose = args.verbose
 
     with open(dns_file) as f:
-       data = f.read()
+        if verbose:
+            print("Opening file: ", dns_file)
+        data = f.read()
 
     domain_list = []
 
     while True:
+        if verbose:
+            print("Parsing json")
         obj, remaining = parse_json(data)
         domain_list.append(obj)
         data = remaining
         if not remaining.strip():
             break
+    if verbose:
+        print("Total records to process:", len(domain_list))
 
     results_list = []
-    currow = 0
+    current_row = 0
     if args.end_at < 1:
-        endrow = len(domain_list)+1
+        last_row = len(domain_list) + 1
     else:
-        endrow = int(args.end_at)
+        if args.end_at < 0:
+            last_row = len(domain_list)
+        else:
+            last_row = int(args.end_at)
+    if verbose:
+        print("Starting at row {} and ending at row {}".format(args.start_at, last_row))
+
     for domain_data in domain_list:
-        currow += 1
-        if currow >= int(args.start_at) and currow <= endrow:
+        current_row += 1
+        if int(args.start_at) <= current_row <= last_row:
             domain_name = list(domain_data.keys())[0]
             results = {}
             for record_type in domain_data[domain_name]:
+                print("Checking record type:", record_type, "for", domain_data[domain_name])
                 ip_address = domain_data[domain_name][record_type]["IP"]
                 if ip_address != "NA":
+                    if verbose:
+                        print(ip_address, "not NA!")
                     results[record_type] = obtain_ip_data(ip_address, verbose=verbose)
                 else:
+                    if verbose:
+                        print(ip_address, "is NA!")
                     results[record_type] = {'CC': 'NA', 'Org': 'NA'}
+                sleep(0.5)
+            if verbose:
+                print(current_row, domain_name)
             results_list.append({domain_name: results})
+            if verbose:
+                print("Added", domain_name, ":", len(results_list), "total records")
+        else:
+            if verbose:
+                print("Skipping row", current_row)
 
-    with open(output_file, 'a') as fp:
-        fp.write('\n'.join(json.dumps(i) for i in results_list) + '\n')
+        if verbose:
+            print("Writing file to", output_file)
+        with open(output_file, 'a') as outfile:
+            outfile.write('\n'.join(json.dumps(i) for i in [{domain_name: results}]) + '\n')

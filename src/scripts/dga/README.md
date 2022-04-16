64acb77428676b04dd14b8b7f21f5c0be627309f
Kevin Gray <grayke@umich.edu>
2022-04-16 09:32:41 -0400

    Current status of DGA model process.

---
+++ src/scripts/dga/README.md
@@ -0,0 +1,14 @@
+These tools are used to generate a DGA dataset.
+
+## PREREQUISITES
+Download a copy of the DGA generation algorithms from https://github.com/baderj/domain_generation_algorithms.
+
+## PROCESS
+1. Run generate_dga_domains.py
+2. Run get_benign_domains.py
+3. Run make_dga_dataset.py
+4. Run build_dga_model.py
+* Command line syntax and examples are noted within each script.
+
+## ADDITIONAL SCRIPTS
+A. example_predict.py: Used to show how the DGA tool and model can be used to integrate into an existing workflow.
---
+++ src/scripts/dga/example_predict.py
@@ -0,0 +1,30 @@
+import argparse
+from dga_functions import dga_prediction 
+
+"""
+Example script that will use a prebuilt model to predict if a domain is a DGA.
+
+Usage: example_predict.py --domain_name test.com --entropy_value 3.1 
+
+Todo:
+* Move functionality to dga_functions and retest
+"""
+
+# Allow to run as a standalone script
+if __name__ == "__main__":
+    # Use Argparse to handle cli inputs
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--domain_name', help='The domain name to predict.')
+    parser.add_argument('--entropy_value', help='The entropy value for the domain name.')
+    parser.add_argument('--verbose', nargs="?", default=False, help='Print verbose information.')
+    args = parser.parse_args()
+    domain_name = args.domain_name
+    entropy_value = args.entropy_value
+    verbose = args.verbose
+
+    results = dga_prediction(domain=domain_name, entropy=entropy_value, verbose=verbose)
+
+    if results == False:
+        print("Domain " + domain_name + " is not a DGA.")
+    else:
+        print("Domain " + domain_name + " is a DGA.")

72eeffea7269a0fb3c55cd8bc5376cec1ea42ba2
liam <ltvanion@umich.edu>
2022-04-19 14:47:46 -0400

    finalized cleaning; added some transformation functions

--- src/scripts/artemis_data.py
+++ src/scripts/artemis_data.py
@@ -1,6 +1,9 @@
 import copy
 import pandas as pd
 import json
+import numpy as np
+
+from collections import Counter
 
 creation_date_cols = ['creation_date', 'creation_date_1', 'creation_date_2', 'creation_date_3', 'creation_date_4']
 updated_date_cols = ['updated_date', 'updated_date_1', 'updated_date_2', 'updated_date_3', 'updated_date_4']
@@ -9,8 +12,10 @@ bad_countries = []
 
 
 def process(row):
-    new_row = {'redacted': False}  # flag whether row has redacted data
+    new_row = {'redacted': 0}  # flag whether row has redacted data
     for k, v in row.items():  # iterate through the row's items
+        if k == 'name_servers' and isinstance(v, str):
+            v = [x.strip() for x in v.split("\n")]
         if isinstance(v, list):  # of the value is a list
             if 'REDACTED FOR PRIVACY' not in v:  # if it wasn't redacted
                 if k in ['creation_date', 'emails', 'expiration_date', 'name_servers', 'status',
@@ -21,7 +26,7 @@ def process(row):
                     vcomb = ";".join(v)  # concatenate the strings
                     new_row.update({k: vcomb})
             else:  # data was redacted
-                new_row['redacted'] = True  # flag it as redadted
+                new_row['redacted'] = 1  # flag it as redacted
                 vcopy = copy.deepcopy(v)  # make a copy of the data
                 while vcopy:  # while there's still data
                     val = vcopy.pop()  # pop one off
@@ -31,7 +36,7 @@ def process(row):
             continue  # skip it
         elif isinstance(v, str):  # if it's a string
             if v == 'REDACTED FOR PRIVACY':  # and it's redacted
-                new_row['redacted'] = True  # flag as redacted
+                new_row['redacted'] = 1  # flag as redacted
             new_row.update({k: v})
         else:  # if none of the above, just include it
             new_row.update({k: v})
@@ -106,6 +111,8 @@ def clean_country(country):
                 c = parts[0]
             elif parts[0] == 'GB' and parts[1] == 'UK':
                 c = 'GB'
+            elif parts[0] == 'CY' and parts[1] == 'CYPRUS':
+                c = 'CY'
             else:
                 bad_countries.append(c)
         elif "UNITED STATES" in c or "HERNDON" in c:
@@ -118,7 +125,7 @@ def clean_country(country):
             c = "CY"
         else:
             bad_countries.append(c)
-    return c
+    return c.lower()
 
 
 def clean_dates(dt):
@@ -180,10 +187,136 @@ def set_expiration_date(row):
     return row
 
 
+def check_for_changed_domain_name(row):
+    ans = False
+    if pd.notna(row.domain_name):
+        parts = row.domain_name.split(";")
+        if len(parts) > 1:
+            part1 = parts[0].lower()
+            part2 = parts[1].lower()
+            ans = part1 == part2
+    return int(ans)
+
+
+def get_ns_cols(num=16):
+    cols = []
+    prefix = 'name_servers_'
+    for i in range(0, num):
+        field = prefix + str(i + 1)
+        cols.append(field)
+    return cols
+
+
+def count_name_servers(row):
+    num_ns = 0
+    for col in get_ns_cols():
+        if pd.notna(row[col]):
+            num_ns += 1
+    return num_ns
+
+
+def check_name_servers(row):
+    ns_suffixes = set()
+    for col in get_ns_cols():
+        if pd.notna(row[col]):
+            domain = row[col]
+            parts = domain.split(".")
+            suffix = ".".join(parts[1:]).lower()
+            ns_suffixes.add(suffix)
+    return len(ns_suffixes)
+
+
+def find_main_ns_domain(row):
+    domains = Counter()
+    for col in get_ns_cols():
+        if pd.notna(row[col]):
+            domain = row[col].split(".")[-2]
+            domains[domain] += 1
+    mc = domains.most_common(1)
+    if len(mc) > 0:
+        return mc[0][0].lower()
+    return pd.NA
+
+
+def get_status_cols():
+    cols = ['status']
+    prefix = "status_"
+    for i in range(0, 12):
+        cols.append(prefix + str(i + 1))
+    return cols
+
+
+def mark_status_flags(row):
+    valid_statuses = ['serverDeleteProhibited', 'clientDeleteProhibited', 'serverRenewProhibited',
+                      'clientRenewProhibited', 'clientTransferProhibited'
+                      ]
+    row_statuses = []
+    for col in get_status_cols():
+        if pd.notna(row[col]):
+            row_statuses.append(row[col])
+    new_statuses = {k: 0 for k in valid_statuses}
+    for status in valid_statuses:
+        for row_status in row_statuses:
+            if status in row_status:
+                new_statuses[status] = 1
+    for status in valid_statuses:
+        row[status] = new_statuses[status]
+    return row
+
+
+def clean_dnssec(val):
+    if pd.isna(val):
+        return "DNSSEC_NA"
+    if ";" in val:
+        parts = val.split(";")
+        part1 = parts[0]
+        part2 = parts[1]
+        if part1.lower() == part2.lower():
+            return part1.lower()
+    return val
+
+
+def get_email_cols(num=6):
+    cols = ['emails']
+    prefix = "emails_"
+    for i in range(0, num):
+        cols.append(prefix + str(i + 1))
+    return cols
+
+
+def count_emails(row):
+    num_emails = 0
+    for col in get_email_cols():
+        if pd.notna(row[col]):
+            num_emails += 1
+    return num_emails
+
+
+def find_email_domains(row):
+    email_domains = set()
+    for col in get_email_cols():
+        if pd.notna(row[col]):
+            email = row[col]
+            email_parts = email.split("@")
+            if len(email_parts) > 1:
+                email_domains.add(email_parts[1])
+            else:
+                email_domains.add(email_parts[0])
+    domain_str = ";".join(email_domains)
+    if domain_str:
+        return domain_str
+    else:
+        return "EMAIL_NAN"
+
+
 def clean_data(df):
     clean_df = df.copy(deep=True)  # make a copy of the df
-    clean_df = clean_df.dropna(subset=['redacted'])  # drop any where redacted is NaN; those don't contain whois record
-    clean_df['country'] = clean_df.country.fillna("ZZ")  # ZZ is no country
+    # drop any where redacted is NaN; those don't contain whois record
+    clean_df = clean_df.dropna(subset=['redacted', 'domain_name'])
+    # drop any columns that are completely empty
+    clean_df = clean_df.dropna(how='all', axis='columns')
+    # Clean country
+    clean_df['country'] = clean_df.country.fillna("zz")  # ZZ is no country
     clean_df['country'] = clean_df.country.apply(clean_country)
     print(f"{len(bad_countries)} records had countries that are ambiguous: {bad_countries}")
     for col in creation_date_cols + updated_date_cols + expiration_date_cols:
@@ -205,6 +338,39 @@ def clean_data(df):
     expiration_date_cols_to_drop.remove(expiration_date_cols[0])
     clean_df = clean_df.drop(columns=expiration_date_cols_to_drop)
     clean_df['days_until_expiration'] = clean_df.expiration_date.apply(
-                        lambda dt: pd.NA if pd.isna(dt) else (dt - pd.Timestamp.today().date()).days
+        lambda dt: pd.NA if pd.isna(dt) else (dt - pd.Timestamp.today().date()).days
     )
+    clean_df['has_multiple_domain_names'] = clean_df.domain_name.apply(lambda x: ";" in x if pd.notna(x) else False)
+    clean_df['has_multiple_domain_names'] = clean_df['has_multiple_domain_names'].astype(int)
+    clean_df['multiple_domain_names_match'] = clean_df.apply(check_for_changed_domain_name, axis=1)
+    clean_df = clean_df.drop(columns='domain_name')
+    # Process name servers
+    clean_df['number_name_servers'] = clean_df.apply(count_name_servers, axis=1)
+    clean_df['num_different_ns_domains'] = clean_df.apply(check_name_servers, axis=1)
+    clean_df['main_name_server_domain'] = clean_df.apply(find_main_ns_domain, axis=1)
+    # Process Status
+    clean_df = clean_df.apply(mark_status_flags, axis=1)
+    clean_df = clean_df.drop(columns=get_status_cols())
+    # Process Registrant Contact Name
+    clean_df['registrant_contact_name'] = np.where(clean_df['registrant_contact_name'].isnull(), 0, 1)
+    clean_df['registrar'] = clean_df.registrar.fillna('REGISTRAR_NAN').apply(str.lower)
+    # Process dnssec
+    clean_df['dnssec'] = clean_df.dnssec.apply(clean_dnssec).apply(str.lower)
+    # Process emails
+    clean_df['num_emails'] = clean_df.apply(count_emails, axis=1)
+    clean_df['email_domains'] = clean_df.apply(find_email_domains, axis=1).apply(str.lower)
+    clean_df['num_email_domains'] = clean_df.email_domains.apply(
+        lambda x: len(x.split(";")) if x != "EMAIL_NAN" else 0
+    )
+    # Fill NAs
+    # drop any columns with fewer than 300 rows having data
+    clean_df = clean_df.dropna(how='any', thresh=300, axis='columns')
+    clean_df['org'] = clean_df.org.fillna("ORG_NAN").apply(str.lower)
+    clean_df['state'] = clean_df.state.fillna("STATE_NAN").apply(str.lower)
+    clean_df['whois_server'] = clean_df.whois_server.fillna("WHOIS_NAN").apply(str.lower)
+    clean_df['address'] = clean_df.address.fillna('ADDRESS_NAN').apply(str.lower)
+    clean_df['city'] = clean_df.city.fillna("CITY_NAN").apply(str.lower)
+    clean_df['name'] = clean_df.name.fillna("NAME_NAN").apply(str.lower)
+    clean_df['zipcode'] = clean_df.zipcode.fillna("ZIP_NAN")
+    clean_df['zipcode'] = clean_df.zipcode.astype(str).apply(str.lower)
     return clean_df

e333a760470c80e9e66649dc25fe213aa1d5c499
liam <ltvanion@umich.edu>
2022-04-19 14:49:47 -0400

    adding graph/network data; change to final_whois_data;

--- src/scripts/make_dataset.py
+++ src/scripts/make_dataset.py
@@ -11,7 +11,7 @@ from artemis_data import load_datafile, clean_data
 whois_data_file_suffix = '_whois_data.txt'
 entropy_data_file_suffix = '_entropy_data.txt'
 ip_data_file_suffix = '_ip_data.txt'
-final_data_filename = 'better_whois_data.csv'
+final_data_filename = 'final_whois_data.csv'
 
 
 @click.command()
@@ -31,18 +31,25 @@ def main(input_filepath, output_filepath):
     benign_ip_df = load_datafile(f"{input_filepath}/benign{ip_data_file_suffix}", filetype='ip')
     benign_merged_df = pd.merge(benign_domain_df, benign_entropy_df, on='domain')
     benign_merged_df = pd.merge(benign_merged_df, benign_ip_df, on='domain')
-    benign_merged_df['malicious'] = False
+    benign_merged_df = benign_merged_df.drop_duplicates(subset='domain', keep='last')
+    benign_merged_df['malicious'] = 0
     logger.info(f"Loading malicious data from {input_filepath}.")
     malicious_domain_df = load_datafile(f"{input_filepath}/malicious{whois_data_file_suffix}", filetype='whois')
     malicious_entropy_df = load_datafile(f"{input_filepath}/malicious{entropy_data_file_suffix}", filetype='entropy')
     malicious_merged_df = pd.merge(malicious_domain_df, malicious_entropy_df, on='domain')
     malicious_ip_df = load_datafile(f"{input_filepath}/malicious{ip_data_file_suffix}", filetype='ip')
     malicious_merged_df = pd.merge(malicious_merged_df, malicious_ip_df, on='domain')
-    malicious_merged_df['malicious'] = True
+    malicious_merged_df = malicious_merged_df.drop_duplicates(subset='domain', keep='last')
+    malicious_merged_df['malicious'] = 1
     logger.info("Concatenating malicious and benign data files.")
     interim_df = pd.concat([benign_merged_df, malicious_merged_df])
+    logger.info("Adding the graph community data.")
+    graph_df = pd.read_csv(f"{output_filepath}/graph_community_features.csv")
+    graph_df = graph_df.rename(columns={"DomainRecord": 'domain'})
+    graph_df = graph_df.drop(columns='malicious_ratio')
+    interim2_df = pd.merge(interim_df, graph_df, on='domain')
     logger.info("Cleaning combined dataframe.")
-    final_df = clean_data(interim_df)
+    final_df = clean_data(interim2_df)
     outfile = f"{output_filepath}/{final_data_filename}"
     final_df.to_csv(outfile, index=False)
     logger.info(f"Wrote merged datafile to {outfile}.")

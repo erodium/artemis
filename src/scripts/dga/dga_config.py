993df2a6d2aacef970262e9f0a833cefa483809b
liam <ltvanion@umich.edu>
2022-04-24 04:20:35 -0400

    adding relative pathing

--- src/scripts/dga/dga_config.py
+++ src/scripts/dga/dga_config.py
@@ -1,3 +1,5 @@
-ngram_size = 4 
-dga_model_file = 'models/dga_model.sav' 
+import os.path as osp
+HERE = osp.dirname(osp.abspath(__file__))
+ngram_size = 4
+dga_model_file = HERE+'/../../../models/dga_model.sav'
 masked_ngram_values = ['n', 's', 'v', 'c']
